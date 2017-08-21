066d051034885a0491d8fbc61356964532946787
Unknown <julien.ledoux@lepoles.com>
2017-08-21 16:59:00 +0200

    21-08-2017

--- Cours JavaScript/jQuery/04_slider/js/slider.js
+++ Cours JavaScript/jQuery/04_slider/js/slider.js
@@ -8,22 +8,33 @@ $(function(){
 
         var containerWidth = slides.length * 400;
 
+        var isMoving = false; // variable de blocage pour savoir si l'animation est en cours ou non
+
         container.css("width", containerWidth);
 
         // on aoute les écouteurs d'événements pour gérer la navigation
         $(this).find(".btn-prev").on("click", function(e){
             e.preventDefault();
             // alert('backward');
-            if (container.position().left < 0) {
-                container.animate({"left" : "+=400"});
+            if (container.position().left < 0 && !isMoving) {
+                isMoving = true;// l'animation commence
+                container.animate({"left" : "+=400"}, function() {
+                    // fonction de callback de la fonction animate
+                    alert('fini');
+                    isMoving = false;
+                });
             }
         });
+
 
         $(this).find(".btn-next").on("click", function(e){
             e.preventDefault();
             // alert('forward');
-            if (container.position().left > -(slides.length - 1) * 400) {
-                container.animate({"left" : "-=400"});
+            if (container.position().left > -(slides.length - 1) * 400 && !isMoving) {
+                isMoving = true;
+                container.animate({"left" : "-=400"}, function(){
+                    isMoving = false;
+                });
             }
         });
     });
