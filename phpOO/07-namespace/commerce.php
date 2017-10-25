c830cd5e0fc97c78ea3fb323b2360b4e73ed1612
JulienL23 <julien.ledoux@lepoles.com>
2017-10-25 16:55:48 +0200

    25-10-2017

---
+++ phpOO/07-namespace/commerce.php
@@ -0,0 +1,26 @@
+<?php
+namespace Commercial;
+class Commande
+{
+    public $nbCommande = 1;
+}
+
+namespace Commerce2;
+class Produit
+{
+    public $nbProduits = 2;
+}
+
+namespace Commerce3;
+class Panier
+{
+    public $nbProduits = 3;
+}
+
+class Produit
+{
+    public $nbProduits = 33;
+}
+
+
+ ?>
---
+++ phpOO/08-autoload/autoload.php
@@ -0,0 +1,18 @@
+<?php
+
+function inclusionAutomatique($nomDeLaClass)
+{
+    include_once($nomDeLaClass . '.class.php');
+    echo 'On passe dans inclusionAutomatique. <br>';
+    echo "require($nomDeLaClass.class.php);<hr>";
+}
+
+spl_autoload_register('inclusionAutomatique');
+
+// Permet d'éxécuter une fonction lorsque l'interpréteur voit passer un "new" dans le code. Le nom à droite du "New" (nom de la class) est passé automatiquement en argument à cette fonction.
+
+$a = new A;
+$b = new B;
+$c = new C;
+$d = new D;
+ ?>
